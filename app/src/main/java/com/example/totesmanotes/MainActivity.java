a3b090f9a3eae08f137f6ef200d5235a2c144e99
Esther Jang <estherj@andrew.cmu.edu>
2020-02-15 02:25:40 -0500

    Added tutorial code

--- app/src/main/java/com/example/totesmanotes/MainActivity.java
+++ app/src/main/java/com/example/totesmanotes/MainActivity.java
@@ -4,6 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.Header;
+import org.json.JSONObject;
+
 import static java.sql.DriverManager.println;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +26,115 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         println("hi");
+    }
+
+    // from source:
+    // https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/quickstarts/java-hand-text
+
+    // **********************************************
+    // *** Update or verify the following values. ***
+    // **********************************************
+
+    // Add your Computer Vision subscription key and endpoint to your environment variables.
+    // After setting, close and then re-open your command shell or project for the changes to take effect.
+    static String subscriptionKey = System.getenv("0e912c522fac4c549f628512bd5df1a0");
+    static String endpoint = ("https://eastus.api.cognitive.microsoft.com");
+
+    private static final String uriBase = endpoint +
+            "vision/v2.1/read/core/asyncBatchAnalyze";
+
+    private static final String imageToAnalyze =
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/" +
+                    "Cursive_Writing_on_Notebook_paper.jpg/800px-Cursive_Writing_on_Notebook_paper.jpg";
+
+    public static void main(String[] args) {
+        CloseableHttpClient httpTextClient = HttpClientBuilder.create().build();
+        CloseableHttpClient httpResultClient = HttpClientBuilder.create().build();
+
+        try {
+            // This operation requires two REST API calls. One to submit the image
+            // for processing, the other to retrieve the text found in the image.
+
+            URIBuilder builder = new URIBuilder(uriBase);
+
+            // Prepare the URI for the REST API method.
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request body.
+            StringEntity requestEntity =
+                    new StringEntity("{\"url\":\"" + imageToAnalyze + "\"}");
+            request.setEntity(requestEntity);
+
+            // Two REST API methods are required to extract text.
+            // One method to submit the image for processing, the other method
+            // to retrieve the text found in the image.
+
+            // Call the first REST API method to detect the text.
+            HttpResponse response = httpTextClient.execute(request);
+
+            // Check for success.
+            if (response.getStatusLine().getStatusCode() != 202) {
+                // Format and display the JSON error message.
+                HttpEntity entity = response.getEntity();
+                String jsonString = EntityUtils.toString(entity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("Error:\n");
+                System.out.println(json.toString(2));
+                return;
+            }
+
+            // Store the URI of the second REST API method.
+            // This URI is where you can get the results of the first REST API method.
+            String operationLocation = null;
+
+            // The 'Operation-Location' response header value contains the URI for
+            // the second REST API method.
+            Header[] responseHeaders = response.getAllHeaders();
+            for (Header header : responseHeaders) {
+                if (header.getName().equals("Operation-Location")) {
+                    operationLocation = header.getValue();
+                    break;
+                }
+            }
+
+            if (operationLocation == null) {
+                System.out.println("\nError retrieving Operation-Location.\nExiting.");
+                System.exit(1);
+            }
+
+            // If the first REST API method completes successfully, the second
+            // REST API method retrieves the text written in the image.
+            //
+            // Note: The response may not be immediately available. Text
+            // recognition is an asynchronous operation that can take a variable
+            // amount of time depending on the length of the text.
+            // You may need to wait or retry this operation.
+
+            System.out.println("\nText submitted.\n" +
+                    "Waiting 10 seconds to retrieve the recognized text.\n");
+            Thread.sleep(10000);
+
+            // Call the second REST API method and get the response.
+            HttpGet resultRequest = new HttpGet(operationLocation);
+            resultRequest.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            HttpResponse resultResponse = httpResultClient.execute(resultRequest);
+            HttpEntity responseEntity = resultResponse.getEntity();
+
+            if (responseEntity != null) {
+                // Format and display the JSON response.
+                String jsonString = EntityUtils.toString(responseEntity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("Text recognition result response: \n");
+                System.out.println(json.toString(2));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
